67411d4e24a76cf06da9a032763616a6c9da1cac
Tim <timkin99@mail.ru>
2024-03-11 19:04:41 +0700

    refactor: Refactored vendors API key validation process
    Vendors API key validation process has been refactored in the app. The validation now runs concurrently for each vendor which should improve overall performance. Also, the methods to get vendors with expired keys and create notifications have been modified to handle each vendor individually, thus improving the readability and maintainability of the code.

--- app/internal/__init__.py
+++ app/internal/__init__.py
@@ -1,3 +1,4 @@
+import asyncio
 import time
 
 from internal.objects.vendor import VendorHandler, VendorModel
@@ -17,8 +18,7 @@ class ExpiredApiKeyNotifierApplication:
         start = time.time()
         log.info(f"Starting validating vendors api keys")
         vendors = await self._get_vendors()
-        vendors_with_expired_api_key = await self._get_vendors_with_expired_api_key(vendors)
-        await self._create_notifications(vendors_with_expired_api_key)
+        await self._run_processing(vendors)
         log.info(f"Finished validating vendors api keys: {time.time() - start:.2f} seconds")
 
     async def _get_vendors(self) -> list[VendorModel]:
@@ -26,11 +26,30 @@ class ExpiredApiKeyNotifierApplication:
         log.info(f"Received vendors {len(vendors)}")
         return vendors
 
-    async def _get_vendors_with_expired_api_key(self, vendors: list[VendorModel]) -> list[VendorModel]:
-        vendors_with_expired_api_key = await self._checker.get_expired_api_keys(vendors)
-        log.info(f"Vendors with expired api keys: {len(vendors_with_expired_api_key)}")
-        return vendors_with_expired_api_key
+    async def _run_processing(self, vendors: list[VendorModel]) -> None:
+        amount = 0
+        tasks = []
+        for vendor in vendors:
+            tasks.append(self._validate_vendor_api_key(vendor))
 
-    async def _create_notifications(self, vendors: list[VendorModel]) -> None:
-        notification_amount = await self._notification_creator.handle_vendors_with_expired_api_key(vendors)
+        for future in asyncio.as_completed(tasks):
+            vendor_with_expired_key = await future
+            if vendor_with_expired_key is None:
+                continue
+            amount += 1
+            await self._create_notifications_for_vendor(vendor_with_expired_key)
+
+        log.info(f"Finished validating vendors api keys. Vendors with expired keys: {amount}")
+
+    async def _validate_vendor_api_key(self, vendor: VendorModel) -> VendorModel | None:
+        vendor_with_expired_key = await self._checker.get_expired_api_keys(vendor)
+
+        if vendor_with_expired_key is None:
+            log.debug(f"Vendor '{vendor.supplier_id}' has correct api key.")
+        else:
+            log.info(f"Vendor '{vendor.supplier_id}' has expired api key.")
+        return vendor_with_expired_key
+
+    async def _create_notifications_for_vendor(self, vendor: VendorModel) -> None:
+        notification_amount = await self._notification_creator.handle_vendors_with_expired_api_key(vendor)
         log.info(f"Created notifications: {notification_amount}")
